c83d1b4d66cad528636930fd8229d5d7b367b3e5
Linostar <linux.anas@gmail.com>
2016-02-27 02:56:31 -0500

    Used a docker network to connect plexus with anope

--- anope2/start_services.sh
+++ anope2/start_services.sh
@@ -1,10 +1,4 @@
 #!/bin/bash
 
-if [ -n "$SERVER_IP" ]; then
-  sed -i "s/server_ip_here/$SERVER_IP/g" /var/ircd/services/conf/services.conf
-else
-  sed -i "s/server_ip_here/127.0.0.1/g" /var/ircd/services/conf/services.conf
-fi
-
-sudo -u ircd /var/ircd/services/bin/services -support
-
+sudo -u ircd /var/ircd/services/bin/services
+read
--- plexus4/start_ircd.sh
+++ plexus4/start_ircd.sh
@@ -1,11 +1,5 @@
 #!/bin/sh
 
-if [ -n "$SERVER_IP" ]; then
-  sed -i "s/server_ip_here/$SERVER_IP/g" /var/ircd/etc/ircd.conf
-else
-  sed -i "s/server_ip_here/127.0.0.1/g" /var/ircd/etc/ircd.conf
-fi
-
 sudo -u ircd /var/ircd/bin/ircd
 read
 
