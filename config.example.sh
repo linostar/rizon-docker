bfae60e4dd4b1b7fea041e259968ddd92fae9c7d
Linostar <linux.anas@gmail.com>
2016-02-28 06:40:51 -0500

    Changed variable retrieval method from config.sh

--- config.example.sh
+++ config.example.sh
@@ -1,3 +1,8 @@
 # IRCd and Services owner credentials
 export OWNER_NAME=owner
 export OWNER_PASSWORD=ChangeMe123
+
+# DO NOT REMOVE! VARIABLE RETRIEVAL CODE.
+if [ $# -eq 1 ]; then
+  echo $"$1"
+fi
