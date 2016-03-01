d83ed283eef120f4e322bb3c7ae5cd745aed8aaa
Linostar <linux.anas@gmail.com>
2016-03-01 16:43:28 -0500

    multiple fixes

--- acid2/start_acid.sh
+++ acid2/start_acid.sh
@@ -1,12 +1,8 @@
 #! /bin/bash
 
 sleep 5
-if [ ! -f /var/ircd/acid/db_configured ]; then
-  sed -i "s/___mysql_ip___/$DB_PORT_3306_TCP_ADDR/g;s/___mysql_port___/$DB_PORT_3306_TCP_PORT/g" /var/ircd/acid/acidictive.yml
-  sed -i "s/___mysql_ip___/$DB_PORT_3306_TCP_ADDR/g" /var/ircd/acid/pypsd.yml
-  touch /var/ircd/acid/db_configured
-fi
 
-export LD_PRELOAD=/usr/lib64/libpython2.7.so
+export LD_PRELOAD=/usr/local/lib/libpython2.7.so
 cd /var/ircd/acid; ./run.sh
+tail -f /var/ircd/acid/geoserv.log
 read
--- mysqld/start_db.sh
+++ mysqld/start_db.sh
@@ -6,7 +6,7 @@ sudo -u mysql /usr/libexec/mysqld &
 if [ ! -f db_created ]; then
   sudo -u mysql mysql_install_db
   sleep 5
-  mysql -u root -e "create database acidcore; create database pypsd; grant all privileges on acidcore.* to 'acid'@'%' identified by 'moo'; grant all privileges on pypsd.* to 'acid'@'%' identified by 'moo'; flush privileges;"
+  mysql -u root -e "create database acidcore; create database pypsd; grant all privileges on acidcore.* to 'acid'@'localhost' identified by 'moo'; grant all privileges on pypsd.* to 'acid'@'localhost' identified by 'moo'; flush privileges;"
   mysql -u root -D acidcore < acidcore.sql
   mysql -u root -D acidcore -e "insert into access values (\"$OWNER_NAME\", '1', '');"
   touch db_created
