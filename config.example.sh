707aabd94738eb6a6a8f60d5f13f3f5e4ef5e1bb
Linostar <linux.anas@gmail.com>
2016-03-11 13:34:54 -0500

    Rewrote rizon-docker.sh and added more variables to config.sh

--- config.example.sh
+++ config.example.sh
@@ -9,19 +9,23 @@ export OWNER_PASSWORD=ChangeMe123
 export NUMBER_OF_SERVERS=1
 
 # Main IRCD server, always with index 0
-# possible values for *_IRCD: plexus3, plexus4
+# possible values for SERVER_*_IRCD: plexus3, plexus4
 export SERVER_0_IRCD=plexus4
-# possible values for *_SERVICES: none, anope1, anope2
-# note that the anope1 container is not full-featured yet and therefore not recommended
+# possible values for SERER_0_SERVICES: none, anope1, anope2
+# note that the anope1 container is not full-featured yet, and therefore not recommended
 export SERVER_0_SERVICES=anope2
-# possible values for *_BOTS (can accept comma-separated list): none, acid, moo
-export SERVER_0_BOTS='acid'
+# possible values for SERVER_0_ACID: 0, 1 (1 means deployed, 0 otherwise)
+export SERVER_0_ACID=1
+# possible values for SERVER_0_MOO: 0, 1 (1 means deployed, 0 otherwise)
+export SERVER_0_MOO=0
 # number of fake users connecting to this server
 export SERVER_0_USERS=0
-# comma-separated list of channels that the fake users will join
-export SERVER_0_USER_CHANNELS='#help'
+# comma-separated list of channels that the fake users, if any, will join
+export SERVER_0_USER_CHANNELS='#chat'
 
 # Other IRCD servers (should reflect the value of $NUMBER_OF_SERVERS)
 export SERVER_1_IRCD=plexus3
+export SERVER_1_USERS=0
+export SERVER_1_CHANNELS=''
 # the index of the server to which this server will be directly connected
 export SERVER_1_LINK=0
--- rizon-docker.sh
+++ rizon-docker.sh
@@ -13,6 +13,118 @@ function build_all {
   echo "Containers built."
 }
 
+# Example: build("ircd", "plexus4")
+function build {
+	case "$1" in
+	ircd)
+		if [ $2 -eq "plexus3" -o $2 -eq "plexus4" ]; then
+			cp config.sh "${2}/"
+			docker build -t $2 "./$2"
+			rm "${2}/config.sh"
+			echo "Container \'$2\' is built."
+		else
+			echo "Error: container \'$2\' does not exist."
+		fi
+		;;
+	esac
+}
+
+# Example: start("ircd", 0, "plexus4")
+function start {
+	case "$1" in
+	ircd)
+		if [ $3 -eq "plexus3" -o $3 -eq "plexus4" ]; then
+			name = "server_$2_ircd"
+			docker run -dit -p "666${2}:666${2}" --name $name $3
+			echo "Container \'$name\' started."
+		else
+			echo "Error: \'$3\' is not a supported ircd type."
+		fi
+		;;
+	esac
+}
+
+# Example: stop("ircd", 0)
+function stop {
+    name = "$server_$2_$1"
+    if [ echo "ircd services acid moo users" | grep -q "\b$1\b" ]; then
+        docker stop $name
+        echo "Container \'$name\' stopped."
+    elif [ $1 -eq "server" ]; then
+        if [ $2 -gt 0 ]; then
+            if [ $SERVER_0_USERS -gt 0 ]; then
+                docker stop server_0_users
+            fi
+            if [ $SERVER_0_ACID -eq 1 ]; then
+                docker stop server_0_acid
+            fi
+            if [ $SERVER_0_MOO -eq 1 ]; then
+                docker stop server_0_moo
+            fi
+            if [ $SERVER_0_SERVICES -ne "none" ]; then
+                docker stop server_0_services
+            fi
+            docker stop server_0_db
+            docker stop server_0_ircd
+        elif [ $2 -eq 0 ]; then
+            declare users="SERVER_${2}_USERS"
+            if [ ${!users} -gt 0 ]; then
+                docker stop server_${2}_users
+            fi
+            docker stop server_${2}_ircd
+        fi
+        echo "All containers of server $2 stopped."
+    elif [ $1 -eq "all" ]; then
+        for i in `seq 0 $[${NUMBER_OF_SERVERS}-1]`; do
+            delete("server", $i)
+        done
+        echo "All servers stopped."
+    else
+        echo "Error: container \'$name\' does not exist."
+    fi
+}
+
+
+# Example: delete("ircd", 0)
+function delete {
+	name = "$server_$2_$1"
+	if [ echo "ircd services acid moo users" | grep -q "\b$1\b" ]; then
+		docker rm -f $name
+		echo "Container \'$name\' deleted."
+	elif [ $1 -eq "server" ]; then
+		if [ $2 -gt 0 ]; then
+			if [ $SERVER_0_USERS -gt 0 ]; then
+				docker rm -f server_0_users
+			fi
+            if [ $SERVER_0_ACID -eq 1 ]; then
+                docker rm -f server_0_acid
+            fi
+            if [ $SERVER_0_MOO -eq 1 ]; then
+                docker rm -f server_0_moo
+            fi
+            if [ $SERVER_0_SERVICES -ne "none" ]; then
+                docker rm -f server_0_services
+            fi
+			docker rm -f server_0_db
+			docker rm -f server_0_ircd
+		elif [ $2 -eq 0 ]; then
+			declare users="SERVER_${2}_USERS"
+			if [ ${!users} -gt 0 ]; then
+				docker rm -f server_${2}_users
+			fi
+			docker rm -f server_${2}_ircd
+		fi
+		echo "All containers of server $2 deleted."
+	elif [ $1 -eq "all" ]; then
+		for i in `seq 0 $[${NUMBER_OF_SERVERS}-1]`; do
+			delete("server", $i)
+		done
+		echo "All servers deleted."
+	else
+		echo "Error: container \'$name\' does not exist."
+	fi
+}
+
 function start_all {
   docker run -dit -p 6660-6670:6660-6670 -p 7000:7000 -p 6697:6697 -p 9999:9999 -p 6633:6633 --name plexus4 plexus4
   docker run -dit --name anope2 --net=container:plexus4 anope2
@@ -34,17 +146,18 @@ function delete_all {
 source config.sh
 
 if [ $# -eq 1 ]; then
-  if [ $1 = "start" ]; then
-    start_all
-  elif [ $1 = "stop" ]; then
-    stop_all
-  elif [ $1 = "build" ]; then
-    build_all
-  elif [ $1 = "delete" ]; then
-    delete_all
-  fi
+	if [ $1 = "start" ]; then
+		start_all
+	elif [ $1 = "stop" ]; then
+		stop_all
+	elif [ $1 = "build" ]; then
+		build_all
+	elif [ $1 = "delete" ]; then
+		delete_all
+	fi
 else
-  echo "No args provided."
-  exit 65
+	echo "Error: No args provided."
+	exit 65
 fi
+
 exit 0
