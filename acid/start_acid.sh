e0e54fc252ee09125386937ede95b1f4a273736f
Linostar <linux.anas@gmail.com>
2016-03-22 05:45:23 -0400

    Fixed connection async issue in acid

--- acid/start_acid.sh
+++ acid/start_acid.sh
@@ -4,4 +4,14 @@ sleep 10
 
 export LD_PRELOAD=/usr/lib/x86_64-linux-gnu/libpython2.7.so
 cd /var/ircd/acid; ./run.sh
+
+sleep 5
+grep -q "Access denied for user 'acid'@'localhost'" /var/ircd/acid/geoserv.log
+
+while [ $? -eq 0 ]; do
+	cd /var/ircd/acid; ./run.sh
+	sleep 5
+	grep -q "Access denied for user 'acid'@'localhost'" /var/ircd/acid/geoserv.log
+done
+
 /bin/bash
